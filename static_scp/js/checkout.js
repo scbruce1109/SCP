250ad0fa006b13399ffd3d193413c4acad537efc
Stephen Bruce <bruce.stephenc@gmail.com>
2019-11-24 19:11:14 -0500

    Combined payment toggle functions in checkout.js

--- static_scp/js/checkout.js
+++ static_scp/js/checkout.js
@@ -34,23 +34,33 @@ function billingAddressToggle() {
   var billingAddressEditBtn = document.getElementById('billing-address-edit-btn');
   var billingAddressForm = document.getElementById('billing-address-form');
   var billingAddressDisplay = document.getElementById('billing-address-display');
+  var paymentEditBtn = document.getElementById('payment-edit-btn');
+  var paymentForm = document.getElementById('payment-form');
+  var paymentDisplay = document.getElementById('payment-display');
 
   billingAddressEditBtn.addEventListener('click', function() {
     billingAddressForm.style.display='block';
     billingAddressDisplay.style.display = 'none';
+    paymentForm.style.display = 'none';
   })
-}
-
-function paymentMethodEdit() {
-  var paymentEditBtn = document.getElementById('payment-edit-btn');
-  var paymentForm = document.getElementById('payment-form');
-  var paymentDisplay = document.getElementById('payment-display');
 
   paymentEditBtn.addEventListener('click', function() {
     paymentForm.style.display='block';
     paymentDisplay.style.display = 'none';
   })
+
 }
+
+// function paymentMethodEdit() {
+//   var paymentEditBtn = document.getElementById('payment-edit-btn');
+//   var paymentForm = document.getElementById('payment-form');
+//   var paymentDisplay = document.getElementById('payment-display');
+//
+//   paymentEditBtn.addEventListener('click', function() {
+//     paymentForm.style.display='block';
+//     paymentDisplay.style.display = 'none';
+//   })
+// }
 
 
 window.addEventListener("load", () => {
